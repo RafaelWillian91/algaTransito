ee779cbc4174f1cf6cd8db3968bf736d5c7b1c8c
rafaw <rafa.willian91@gmail.com>
2024-07-01 22:12:32 -0300

    Adicionado ExceptionHandler global

--- src/main/java/br/algaworks/algatransito/api/exceptionHandler/ApiExceptionHandler.java
+++ src/main/java/br/algaworks/algatransito/api/exceptionHandler/ApiExceptionHandler.java
@@ -1,18 +1,48 @@
 package br.algaworks.algatransito.api.exceptionHandler;
 
 import br.algaworks.algatransito.domain.exception.NegocioException;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+        ProblemDetail problemDetail = ProblemDetail.forStatus(status);
+        problemDetail.setTitle("Um ou mais campos estão inválidos");
+        problemDetail.setType(URI.create("https://camposInvalidos.com/erros/campos-invalidos"));
+
+        Map<String, String> fields = ex.getBindingResult().getAllErrors().stream()
+                .collect(Collectors.toMap(objectError -> ((FieldError) objectError).getField(),
+                        DefaultMessageSourceResolvable::getDefaultMessage));
+
+        problemDetail.setProperty("invalidFields", fields);
+
+        return handleExceptionInternal(ex, problemDetail, headers, status, request);
+    }
 
     @ExceptionHandler(NegocioException.class)
     public ResponseEntity<String> capturar(NegocioException e){
         return ResponseEntity.badRequest().body(e.getMessage());
     }
+
+
 
 
 }
